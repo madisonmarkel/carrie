7708db0cd76887e147e5be1d197cb67ea3fc0edf
Madison Markel <mmarkel@lawgical.com>
2020-07-30 00:51:21 -0600

    can't figure out js issue

--- app/assets/javascripts/lib/header.js
+++ app/assets/javascripts/lib/header.js
@@ -1,19 +1,4 @@
-
-// SMOOTH SCROLL
-$(document).ready(function(){
-  // $('a[href^="#"]').on('click',function (e) {
-  //     e.preventDefault();
-
-  //     var target = this.hash;
-  //     var $target = $(target);
-
-  //     $('html, body').stop().animate({
-  //         'scrollTop': $target.offset().top
-  //     }, 900, 'swing', function () {
-  //         window.location.hash = target;
-  //     });
-  // });
-
+$(document).ready(function() {
 // MENU
 function navigation() {
   $('#js_menu_expander').click(function(event) {
@@ -30,6 +15,10 @@ function navigation() {
     $('#js_close_menu').hide();
     $("#js_menu_expander").show();
   });
+  $('#js_show_nav').hide(1000);
+  $('#js_close_menu').hide();
+
+  // $("#js_menu_expander").show();
 
   // $('.js_click_close').click(function(event) {
   //   event.preventDefault();
@@ -45,10 +34,21 @@ function navigation() {
   //   $('#js_close_menu').hide();
   //   $("#js_menu_expander").show();
   // });
-  $('#js_show_nav').hide(1000);
-  $('#js_close_menu').hide();
-  // $("#js_menu_expander").show();
 };
 navigation();
+
+// SMOOTH SCROLL
+// $('a[href^="#"]').on('click',function (e) {
+//     e.preventDefault();
+
+//     var target = this.hash;
+//     var $target = $(target);
+
+//     $('html, body').stop().animate({
+//         'scrollTop': $target.offset().top
+//     }, 900, 'swing', function () {
+//         window.location.hash = target;
+//     });
+// });
 
 });
