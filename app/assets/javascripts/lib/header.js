18aa7d158f1feafd2b934bdd516bb448657b5417
Madison Markel <mmarkel@lawgical.com>
2020-07-30 17:30:15 -0600

    js circle mobile menu and css cleanup

--- app/assets/javascripts/lib/header.js
+++ app/assets/javascripts/lib/header.js
@@ -1,5 +1,10 @@
+// JS WITH TURBOLINKS
 // $(document).on("click", ".this-is-the-thing-you-clicked-on", function(){ // code here }}); instead of $(".this-is-the-thing-you-clicked-on").click(function(){ //code here }});
 // so it's registering the event all the way up on "document", instead of on the element itself, because if that element doesn't exist yet then it'll _never_ trigger
+// OR
+// $( document ).on('turbolinks:load', function() {
+//   console.log("It works on each visit!")
+// })
 
 $(document).ready(function() {
 // MENU
@@ -20,60 +25,40 @@ function navigation() {
   }));
   $('#js_show_nav').hide(1000);
   $('#js_close_menu').hide();
-
-  // $("#js_menu_expander").show();
-
-  // $('.js_click_close').click(function(event) {
-  //   event.preventDefault();
-  //     console.log("close link");
-  //     var target = this.hash;
-  //     var $target = $(target);
-  //     $('html, body').stop().animate({
-  //         'scrollTop': $target.offset().top
-  //     }, 900, 'swing', function () {
-  //         window.location.hash = target;
-  //     });
-  //   $('#js_show_nav').hide(1000);
-  //   $('#js_close_menu').hide();
-  //   $("#js_menu_expander").show();
-  // });
 };
 navigation();
 
-// SMOOTH SCROLL
-// $('a[href^="#"]').on('click',function (e) {
-//     e.preventDefault();
+});
 
-//     var target = this.hash;
-//     var $target = $(target);
-
-//     $('html, body').stop().animate({
-//         'scrollTop': $target.offset().top
-//     }, 900, 'swing', function () {
-//         window.location.hash = target;
-//     });
-// });
-
-
-// EYEBALL INDICATOR
-function eyeball() {
-  // var pathname = window.location.pathname;
-  // console.log(pathname);
-
-  // when pathname == that link then show js_eyeball
-
-  $('.desktop_menu').each(function(){
+// EYEBALL INDICATOR - DESKTOP
+function eyeball_menu_indicator() {
+  $('.js_desktop_menu').each(function(){
+    // gets all the links in the menu
     var menu_hrefs = ($(this).attr('href').split('?'))[0];
-    // console.log(menu_hrefs);
 
+    // does the url match one of the menu_hrefs?
     if(window.location.href.indexOf(menu_hrefs) > -1) {
-      console.log('fdsfds ' + (window.location.href.indexOf(menu_hrefs) > -1))
       console.log(this);
-      $(this).addClass('no_color');
-      // $(this).find('.js_eyeball').removeClass('hide');
+      $(this).next().removeClass('hide');
     }
   })
 }
-eyeball();
 
+// CIRCLE MENU INDICATOR - MOBILE
+function circle_menu_indicator() {
+  $('.js_mobile_menu').each(function(){
+    // gets all the links in the menu
+    var menu_hrefs = ($(this).attr('href').split('?'))[0];
+
+    // does the url match one of the menu_hrefs?
+    if(window.location.href.indexOf(menu_hrefs) > -1) {
+      console.log(this);
+      $(this).next().removeClass('hide');
+    }
+  })
+}
+
+$( document ).on('turbolinks:load', function() {
+  eyeball_menu_indicator();
+  circle_menu_indicator();
 });
