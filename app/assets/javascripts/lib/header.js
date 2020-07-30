c56a12516b75e44af2d86508601b848d3b2884a4
Madison Markel <mmarkel@lawgical.com>
2020-07-29 22:12:36 -0600

    fixed_width and header fix and trying to figure out js issue

--- app/assets/javascripts/lib/header.js
+++ app/assets/javascripts/lib/header.js
@@ -18,30 +18,36 @@ $(document).ready(function(){
 function navigation() {
   $('#js_menu_expander').click(function(event) {
     event.preventDefault();
+    console.log("yay");
     $('#js_show_nav').show(1000);
     $('#js_menu_expander').hide();
     $('#js_close_menu').show();
   });
   $('#js_close_menu').click(function(event) {
     event.preventDefault();
+    console.log("close x");
     $('#js_show_nav').hide(1000);
     $('#js_close_menu').hide();
     $("#js_menu_expander").show();
   });
 
-  $('.js_click_close').click(function(event) {
-    event.preventDefault();
-      var target = this.hash;
-      var $target = $(target);
-      $('html, body').stop().animate({
-          'scrollTop': $target.offset().top
-      }, 900, 'swing', function () {
-          window.location.hash = target;
-      });
-    $('#js_show_nav').hide(1000);
-    $('#js_close_menu').hide();
-    $("#js_menu_expander").show();
-  });
+  // $('.js_click_close').click(function(event) {
+  //   event.preventDefault();
+  //     console.log("close link");
+  //     var target = this.hash;
+  //     var $target = $(target);
+  //     $('html, body').stop().animate({
+  //         'scrollTop': $target.offset().top
+  //     }, 900, 'swing', function () {
+  //         window.location.hash = target;
+  //     });
+  //   $('#js_show_nav').hide(1000);
+  //   $('#js_close_menu').hide();
+  //   $("#js_menu_expander").show();
+  // });
+  $('#js_show_nav').hide(1000);
+  $('#js_close_menu').hide();
+  // $("#js_menu_expander").show();
 };
 navigation();
 
